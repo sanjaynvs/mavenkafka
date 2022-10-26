d988fa6684a7faea4b7e700356ed27d156190046
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-10-26 20:07:28 +0530

    java producer

--- kafka-project/src/main/java/com/sanjay/kafkatest/ProducerDemo.java
+++ kafka-project/src/main/java/com/sanjay/kafkatest/ProducerDemo.java
@@ -15,6 +15,7 @@ public class ProducerDemo {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        //comment changed
 
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
