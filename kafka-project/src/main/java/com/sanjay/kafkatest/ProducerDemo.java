b28d2c2699d8ce101d1be1a112cc650501dc73ec
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-10-26 20:35:37 +0530

    java producer

--- kafka-project/src/main/java/com/sanjay/kafkatest/ProducerDemo.java
+++ kafka-project/src/main/java/com/sanjay/kafkatest/ProducerDemo.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 public class ProducerDemo {
     public static void main(String[] args){
 //        Create Producer properties
+        System.out.println("in producer....");
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
