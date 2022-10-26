3b89b359781af0b23b5b3e1d0da15039d663b210
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-10-26 19:59:27 +0530

    java producer

---
+++ kafka-project/src/main/java/com/sanjay/kafkatest/ProducerDemo.java
@@ -0,0 +1,32 @@
+package com.sanjay.kafkatest;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args){
+//        Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic","hello word");
+        //send data - asynchronous
+        producer.send(record);
+        //flush
+        producer.flush();
+        //flush and close
+        producer.close();
+
+    }
+}
