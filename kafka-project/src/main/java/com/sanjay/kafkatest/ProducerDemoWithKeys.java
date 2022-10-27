a2b26187604477022bf78e2528c45942277597e4
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-10-27 18:31:36 +0530

    java producer

---
+++ kafka-project/src/main/java/com/sanjay/kafkatest/ProducerDemoWithKeys.java
@@ -0,0 +1,58 @@
+package com.sanjay.kafkatest;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoWithKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        Create Producer properties
+        System.out.println("in producer....");
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithKeys.class);
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        //comment changed
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for(int i=0;i<10;i++) {
+
+            String topic = "first topic";
+            String value = "hello world "+ Integer.toString(i);
+            String key = "id_"+Integer.toString(i);
+
+            //create producer record
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+            logger.info("key :"+ key);
+            //send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //executes everytime a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        logger.info("Received new metadata: \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp()
+                        );
+
+                    } else {
+                        logger.error("Error while producing" + e);
+                    }
+                }
+            }).get();
+        }
+        //flush
+        producer.flush();
+        //flush and close
+        producer.close();
+
+    }
+}
