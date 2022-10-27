bb5904210c9ca1337a886cd2cf146648d57fa3e0
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-10-27 18:21:31 +0530

    java producer

---
+++ kafka-project/src/main/java/com/sanjay/kafkatest/ProducerDemoWithCallback.java
@@ -0,0 +1,52 @@
+package com.sanjay.kafkatest;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    public static void main(String[] args){
+//        Create Producer properties
+        System.out.println("in producer....");
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        //comment changed
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for(int i=0;i<10;i++) {
+
+            //create producer record
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello word "+Integer.toString(i));
+            //send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //executes everytime a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        logger.info("Received new metadata: \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp()
+                        );
+
+                    } else {
+                        logger.error("Error while producing" + e);
+                    }
+                }
+            });
+        }
+        //flush
+        producer.flush();
+        //flush and close
+        producer.close();
+
+    }
+}
