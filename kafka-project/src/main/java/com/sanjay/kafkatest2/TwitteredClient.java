731fddc10f5f48df3f4269c8259ab6c4d04ecc67
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-11-03 11:26:40 +0530

    java producer

--- kafka-project/src/main/java/com/sanjay/kafkatest2/TwitteredClient.java
+++ kafka-project/src/main/java/com/sanjay/kafkatest2/TwitteredClient.java
@@ -1,14 +1,21 @@
 package com.sanjay.kafkatest2;
 
 import com.github.redouane59.twitter.TwitterClient;
+import com.github.redouane59.twitter.dto.stream.StreamRules;
 import com.github.redouane59.twitter.dto.tweet.Tweet;
 import com.github.redouane59.twitter.dto.user.User;
+//import com.github.redouane59.twitter.helpers.TweetStreamConsumer;
 import com.github.redouane59.twitter.signature.TwitterCredentials;
+import com.github.redouane59.twitter.IAPIEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class TwitteredClient {
 
     public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(TwitteredClient.class.getName());
 
         TwitterClient twitterClient = new TwitterClient(TwitterCredentials.builder()
                 .accessToken("102343857-PytIbKcqqIug717AKb4VpWwg5zxrzuTf3IaerlfV")
@@ -33,14 +40,51 @@ public class TwitteredClient {
 //        System.out.println(user.getUser().getUrl());
 
 
-        Tweet tweet   = twitterClient.getTweet("1224041905333379073");
-        System.out.println(tweet.getText());
-        System.out.println(tweet.getCreatedAt());
-        System.out.println(tweet.getLang());
-        System.out.println(tweet.getLikeCount());
-        System.out.println(tweet.getRetweetCount());
-        System.out.println(tweet.getReplyCount());
-        System.out.println(tweet.getUser().getName());
+//        Tweet tweet   = twitterClient.getTweet("1224041905333379073");
+//        System.out.println(tweet.getText());
+//        System.out.println(tweet.getCreatedAt());
+//        System.out.println(tweet.getLang());
+//        System.out.println(tweet.getLikeCount());
+//        System.out.println(tweet.getRetweetCount());
+//        System.out.println(tweet.getReplyCount());
+//        System.out.println(tweet.getUser().getName());
+//
+//        //twitterClient.startFilteredStream()
+//
+////        twitterClient.addFilteredStreamRule("#cricket","")
+        StreamRules.StreamRule streamRule = twitterClient.addFilteredStreamRule("#cricket", "");
+        //twitterClient.startFilteredStream()
+//        twitterClient.
+
+            twitterClient.startFilteredStream(new IAPIEventListener() {
+                @Override
+                public void onStreamError(int i, String s) {
+
+                    logger.info("i...."+i);
+//                    System.out.println("i...."+i);
+//                    System.out.println("s...."+s);
+                    logger.info("s...."+s);
+                }
+
+                @Override
+                public void onTweetStreamed(Tweet tweet) {
+                    logger.info("[mention] from:@" + tweet.getUser().getName() + " : " + tweet.getText());
+
+                }
+
+                @Override
+                public void onUnknownDataStreamed(String s) {
+
+                }
+
+                @Override
+                public void onStreamEnded(Exception e) {
+                    System.out.println("ended...");
+
+                }
+            });
+
+
 
     }
 }
