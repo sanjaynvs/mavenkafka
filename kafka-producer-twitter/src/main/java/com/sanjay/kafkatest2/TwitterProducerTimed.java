9af494688a78dfb49f52b310c4f3ddf085b85950
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-11-06 20:38:56 +0530

    java producer

--- kafka-producer-twitter/src/main/java/com/sanjay/kafkatest2/TwitterProducerTimed.java
+++ kafka-producer-twitter/src/main/java/com/sanjay/kafkatest2/TwitterProducerTimed.java
@@ -150,9 +150,9 @@ public class TwitterProducerTimed{
             //...greater than 1.1, so we keep this 5, else needs to be 1
 
             //high throughput producer(at the expense of bit of latency and CPU usage introduced!)
-            properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy");
-            properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");
-            properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));//32 kb batch size
+//            properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy");
+//            properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");
+//            properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));//32 kb batch size
 
 //          Create the producer
             producer = new KafkaProducer<String, String>(properties);
