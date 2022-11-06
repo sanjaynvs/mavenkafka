69863663534c24488d7aef7cf0465f8c8a15eb76
Sanjay Joshi <sanjay.p.joshi@gmail.com>
2022-11-06 19:58:42 +0530

    java producer

--- kafka-consumer-elasticsearch/src/main/java/com/github/sanjay/kafka/tutorial3/ElasticSearchConsumerTest.java
+++ kafka-consumer-elasticsearch/src/main/java/com/github/sanjay/kafka/tutorial3/ElasticSearchConsumerTest.java
@@ -101,6 +101,7 @@ public class ElasticSearchConsumerTest {
                 IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
                 String id = indexResponse.getId();
                 logger.info(id);
+                client.close();
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
